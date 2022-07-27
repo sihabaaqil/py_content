cfa5f92cdf490c80b2c28a081b4be1b85b76dcfa
Aaqil Sihab <aaqilnas@gmail.com>
2022-07-27 17:37:19 +0530

    Arrat

--- Learning/Array_Test.py
+++ Learning/Array_Test.py
@@ -15,3 +15,25 @@ except ValueError as ve:
     print("Value is not in list")
 numbers.pop(-1)
 print(numbers)
+numbers.append(9)
+print(len(numbers))
+print(numbers)
+numbers.extend('8')
+print(numbers)
+index = len(numbers) - 1
+numbers.insert(index, 5)
+print(len(numbers))
+print(index)
+print(numbers)
+
+
+a = [[2, 3]]*2
+a[1][0] = 3
+print(a)
+
+# my_input = [1, 2, 3, 4, 5]
+# print(f'Current Numbers List {my_input}')
+# number = int(input("Please enter a number to be added:\n"))
+# index = int(input(f'Enter the index between 0 and {len(my_input) - 1} to add the given number:\n'))
+# my_input.insert(index, number)
+# print(f'Updated List {my_input}')
