662583148c7565b309cf7d7b909de6059eed8564
Aaqil Sihab <aaqilnas@gmail.com>
2022-07-27 16:17:08 +0530

    Lamba

---
+++ Learning/Lambda.py
@@ -0,0 +1,12 @@
+def myfunc(n):
+  return lambda a : a * n
+
+x = lambda a : a + 10
+print(x(5))
+x = lambda a, b : a * b
+print(x(5, 6))
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+mydoubler = myfunc(6)
+print(mydoubler(11))
+
